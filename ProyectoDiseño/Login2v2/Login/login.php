aeb19ed0866955f1a7b78ca4bc7de10b62d331da
helder <helderguerra18@gmail.com>
2021-06-01 23:09:13 -0500

    login-style

--- ProyectoDiseño/Login2v2/Login/login.php
+++ ProyectoDiseño/Login2v2/Login/login.php
@@ -3,28 +3,21 @@
 include("inc/head.php");
 
 ?>
-
+<link rel="stylesheet" href="/ProyectoDiseño/Login2v2/Login/Css/style.css">
 
 <body>
-	<main class="d-flex w-100">
-		<div class="container d-flex flex-column">
+	<main class="d-flex w-100 cuerpo">
+		<div class="container d-flex flex-column ">
 			<div class="row vh-100">
 				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
 					<div class="d-table-cell align-middle">
-
-						<div class="text-center mt-4">
-							<h1 class="h2">Bienvenido</h1>
-							<p class="lead">
-								Ingrese usuario y Contraseña
-							</p>
-						</div>
 						<div id="mensajeno"></div>
-
-						<div class="card">
+						<div class="card card-edit">
 							<div class="card-body">
 								<div class="m-sm-4">
 									<div class="text-center">
-										<img src="inc/img/avatars/avatar.jpg" alt="Charles Hall" class="img-fluid rounded-circle" width="132" height="132" />
+										<p class="Bienvenido">Bienvenido</p>
+									<!-- <img src="ProyectoDiseño/ProyectoDiseño/Login2v2/Login/inc/img/usuario.png" alt="Charles Hall" class="img-fluid rounded-circle " width="132" height="132" /> -->
 									</div>
 								
 										<div class="mb-3">
@@ -62,6 +55,6 @@ include("inc/head.php");
 	</main>
 
 	<?php 
-include("inc/footer.php");
+
 
 ?>
