a88c4e395c568aec10f55ce6ed58232ba35040a2
helder <helderguerra18@gmail.com>
2021-06-11 10:50:05 -0500

    indexProfesional

--- ProyectoDiseño/Login2v2/Login/inc/js/buscarOferta.js
+++ ProyectoDiseño/Login2v2/Login/inc/js/buscarOferta.js
@@ -1,31 +1,53 @@
 let boton = document.querySelector(".boton");
+let input = document.getElementById("inputBuscarOferta")
 
-boton.addEventListener("click",(e)=>{
-
-  e.preventDefault();
-  let filtroOferta = document.getElementById("inputBuscarOferta").value;
-  let tarjeta = document.querySelector(".row-filtros");
-  let imagen = document.getElementById("imagen");
-  let p = document.getElementById("p-index");
-  var datos;
-
-  if (filtroOferta != "") {
+boton.addEventListener("click", (e) => {
+  let datoClases = recibirDatos();
+  if (datoClases.filtroOferta != "") {
     $.ajax({
       type: "POST",
       dataType: "json",
       url: "Controles/buscarOfertas.php",
       data: {
         accion: "buscar",
-        filtroOferta: filtroOferta,
+        filtroOferta: datoClases.filtroOferta,
       },
       success: function (resp) {
-        
-        imagen.remove();
-        p.remove();
+        removerImagenYParrafo(datoClases);
         var datos = resp;
-        var t;
-        datos.forEach(elemento => {
-          t = `<div class='row  d-flex flex-wrap'>
+        let t;
+        datos.forEach((elemento) => {
+          t = crearTarjeta(elemento);
+          var div = document.createElement("DIV");
+          div.innerHTML = t;
+          datoClases.tarjeta.appendChild(div);
+        });
+      },
+    });
+    e.preventDefault();
+  } else {
+    alert("Ingrese algun valor de busqueda por favor!!");
+  }
+});
+
+function recibirDatos() {
+  let filtroOferta = document.getElementById("inputBuscarOferta").value;
+  let tarjeta = document.querySelector(".row-filtros");
+  let imagen = document.getElementById("imagen");
+  let p = document.getElementById("p-index");
+
+  clases = {
+    filtroOferta: filtroOferta,
+    tarjeta: tarjeta,
+    imagen: imagen,
+    p: p,
+  };
+
+  return clases;
+}
+
+function crearTarjeta(elemento) {
+  return `<div class='row  d-flex flex-wrap'>
             <div class='card mt-3 mb-3 edit-tarjeta-profesional' '>
                <div class='img-tarjeta mb-7'>
                <img src="${elemento.foto}"class='card-img-top img-tarjeta-profesional' alt='...'>
@@ -48,19 +70,9 @@ boton.addEventListener("click",(e)=>{
                </button>
            </div>
             </div>`;
-            var div = document.createElement("DIV");
-            div.innerHTML = t;
-            tarjeta.appendChild(div);})
+}
 
-            $("#inputBuscarOferta").val()="";
-      },
-      
-    });
-  } else {
-    alert("Ingrese algun valor de busqueda por favor!!");
-  }
-})
-
-
-
-
+function removerImagenYParrafo(datoClases) {
+  datoClases.imagen.remove();
+  datoClases.p.remove();
+}
