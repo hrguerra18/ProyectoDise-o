722019f58fd9a10383cafc8916db35b917afd06a
helder <helderguerra18@gmail.com>
2021-06-15 15:51:12 -0500

    Realizando perfil profesional

--- ProyectoDiseño/Login2v2/Login/inc/footer.php
+++ ProyectoDiseño/Login2v2/Login/inc/footer.php
@@ -4,6 +4,7 @@
 <script type="text/javascript" src="inc/js/login.js"></script>
 <script src="http://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script src="inc/js/buscarOferta.js"></script>
+<script src="inc/js/perfilProfesional.js"></script>
 
 </body>
 
---
+++ ProyectoDiseño/Login2v2/Login/inc/js/perfilProfesional.js
@@ -0,0 +1,14 @@
+function ModificarPerfil(){
+    let carreraProfesional = document.getElementById("carreraProfesional").value.trim();
+    let fechaNacimientoProfesional = document.getElementById("fechaNacimientoProfesional").value.trim();
+    let nombreProfesional = document.getElementById("nombreProfesional").value.trim();
+    let apellidoProfesional = document.getElementById("apellidoProfesional").value.trim();
+    let sobreMiProfesional = document.getElementById("sobreMiProfesional").value.trim();
+    let direccionProfesional = document.getElementById("direccionProfesional").value.trim();
+    let telefonoProfesional = document.getElementById("telefonoProfesional").value.trim();
+    let departamentoProfesional = document.getElementById("departamentoProfesional").value.trim();
+    let ciudadProfesional = document.getElementById("ciudadProfesional").value.trim();
+
+
+    
+}
