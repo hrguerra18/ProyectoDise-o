cba5e91362519139294157f213be3d544517020c
helder <helderguerra18@gmail.com>
2021-06-19 02:14:54 -0500

    sdasdas

--- ProyectoDiseño/Login2v2/Login/registrarProfesional.php
+++ ProyectoDiseño/Login2v2/Login/registrarProfesional.php
@@ -7,17 +7,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Css/style-profesional.css">
     <title>Profesional</title>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-	<link href="http://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="http://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
 </head>
 
 <body>
-
     <section class="cuerpo">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navegacion">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
         <form action="" class="formulario">
             <div class="botones">
                 <ul class="ul-botones">
@@ -25,7 +41,7 @@
                         <a class=" a-profesional " href="registrarProfesional.php"> COMO PROFESIONAL</a>
                     </li>
                     <li class="li-empresa li-profesional-opacity">
-                    <a class=" a-profesional " href="registrarEmpresa.php"> COMO EMPRESA</a>
+                        <a class=" a-profesional " href="registrarEmpresa.php"> COMO EMPRESA</a>
                     </li>
                 </ul>
             </div>
@@ -34,45 +50,45 @@
             <p class="parrafo-2">Podras hacer uso de la plataforma</p>
 
             <div class="inputs">
-            <div class="grupo-nom-ape">
+                <div class="grupo-nom-ape">
                     <div class="formulario-nombre borde-div">
-                        <input class="input-nombre form-control"autocomplete="off" type="text" name="identidad" id="identidad" placeholder="Identidad" required>
+                        <input class="input-nombre form-control" autocomplete="off" type="text" name="identidad" id="identidad" placeholder="Identidad" required>
                     </div>
 
                     <div class="formulario-apellido borde-div">
-                        <input class="input-apellido form-control" autocomplete="off"type="text" name="foto" id="foto" placeholder="Foto" required>
+                        <input class="input-apellido form-control" autocomplete="off" type="text" name="foto" id="foto" placeholder="Foto" required>
                     </div>
 
                 </div>
 
                 <div class="grupo-nom-ape">
                     <div class="formulario-nombre borde-div">
-                        <input class="input-nombre form-control" autocomplete="off"type="text" name="nombre" id="nombre" placeholder="Nombre" required>
+                        <input class="input-nombre form-control" autocomplete="off" type="text" name="nombre" id="nombre" placeholder="Nombre" required>
                     </div>
 
                     <div class="formulario-apellido borde-div">
-                        <input class="input-apellido form-control" autocomplete="off"type="text" name="apellido" id="apellido" placeholder="Apellido" required>
+                        <input class="input-apellido form-control" autocomplete="off" type="text" name="apellido" id="apellido" placeholder="Apellido" required>
                     </div>
 
                 </div>
 
                 <div class="grupo-nom-ape">
                     <div class="formulario-nombre borde-div">
-                        <input class="input-nombre form-control" autocomplete="off"type="text" name="direccion" id="direccion" placeholder="Direccion" required>
+                        <input class="input-nombre form-control" autocomplete="off" type="text" name="direccion" id="direccion" placeholder="Direccion" required>
                     </div>
 
                     <div class="formulario-apellido borde-div">
-                        <input class="input-apellido form-control"autocomplete="off"type="number" name="telefono" id="telefono" placeholder="Telefono" required>
+                        <input class="input-apellido form-control" autocomplete="off" type="number" name="telefono" id="telefono" placeholder="Telefono" required>
                     </div>
 
                 </div>
 
                 <div class="formulario-correo borde-correo">
-                    <input class="input-formulario form-control" autocomplete="off"type="email" name="email" id="email" placeholder="Correo electronico" required>
+                    <input class="input-formulario form-control" autocomplete="off" type="email" name="email" id="email" placeholder="Correo electronico" required>
                 </div>
 
                 <div class="formulario-contra borde-correo">
-                    <input class="input-formulario form-control" autocomplete="off"type="password" name="contraseña" id="contraseña" placeholder="Contraseña" required>
+                    <input class="input-formulario form-control" autocomplete="off" type="password" name="contraseña" id="contraseña" placeholder="Contraseña" required>
                 </div>
             </div>
 
